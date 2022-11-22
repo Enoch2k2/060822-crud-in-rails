21ac0cd9bf64ede2ffc5b7066216b41358336d72
Enoch2k2 <enoch2k2@hotmail.com>
2022-11-22 17:06:06 -0600

    added validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,14 @@
 class Movie < ApplicationRecord
   validates :title, presence: true, uniqueness: true
+  validates :release_date, presence: true
+  validates :genre, presence: true
+  validates :description, presence: true
+  validate :validate_date_of_release_date
+  ### Release Date can only be set to today or previous
+
+  def validate_date_of_release_date
+    if self.release_date && self.release_date > Date.today
+      errors.add(:release_date, "can't be in the future")
+    end
+  end
 end
